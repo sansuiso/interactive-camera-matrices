998851a25a7bf2dfeeb1a8836e52cec894e8e886
Emmanuel D'ANGELO <emmanuel.dangelo@gmail.com>
2015-04-04 16:25:12 +0200

    Toggle OpenGL 3.2 Core Profile

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@ int main(int argc, char *argv[])
     format.setDepthBufferSize(24);
     QSurfaceFormat::setDefaultFormat(format);
 
+    format.setVersion(3, 2);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+
     MainWindow w;
     w.resize(w.sizeHint());
     w.show();
