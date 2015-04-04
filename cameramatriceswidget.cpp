220febef3c4716793ace53368f14385b4f14f1e6
Emmanuel D'ANGELO <emmanuel.dangelo@gmail.com>
2015-04-04 18:43:19 +0200

    Get a drawing starting point by degrading everything to draw a simple triangle.

--- cameramatriceswidget.cpp
+++ cameramatriceswidget.cpp
@@ -5,33 +5,53 @@
 #include "camera.h"
 #include "cameramatriceswidget.h"
 
+static GLfloat const data3d[] = {
+    0.0f, 1.0f, 0.0f,
+    -1.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+};
+
+//static const char *vertexShaderSourceCore =
+//    "#version 150\n"
+//    "in vec4 vertex;\n"
+//    "in vec3 normal;\n"
+//    "out vec3 vert;\n"
+//    "out vec3 vertNormal;\n"
+//    "uniform mat4 projMatrix;\n"
+//    "uniform mat4 mvMatrix;\n"
+//    "uniform mat3 normalMatrix;\n"
+//    "void main() {\n"
+//    "   vert = vertex.xyz;\n"
+//    "   vertNormal = normalMatrix * normal;\n"
+//    "   gl_Position = projMatrix * mvMatrix * vertex;\n"
+//    "}\n";
+
+//static const char *fragmentShaderSourceCore =
+//    "#version 150\n"
+//    "in highp vec3 vert;\n"
+//    "in highp vec3 vertNormal;\n"
+//    "out highp vec4 fragColor;\n"
+//    "uniform highp vec3 lightPos;\n"
+//    "void main() {\n"
+//    "   highp vec3 L = normalize(lightPos - vert);\n"
+//    "   highp float NL = max(dot(normalize(vertNormal), L), 0.0);\n"
+//    "   highp vec3 color = vec3(0.39, 1.0, 0.0);\n"
+//    "   highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);\n"
+//    "   fragColor = vec4(1, 1, 1, 1);\n/* vec4(col, 1.0);\n*/"
+//    "}\n";
+
 static const char *vertexShaderSourceCore =
     "#version 150\n"
-    "in vec4 vertex;\n"
-    "in vec3 normal;\n"
-    "out vec3 vert;\n"
-    "out vec3 vertNormal;\n"
-    "uniform mat4 projMatrix;\n"
-    "uniform mat4 mvMatrix;\n"
-    "uniform mat3 normalMatrix;\n"
+    "in vec3 vertex;\n"
     "void main() {\n"
-    "   vert = vertex.xyz;\n"
-    "   vertNormal = normalMatrix * normal;\n"
-    "   gl_Position = projMatrix * mvMatrix * vertex;\n"
+    "   gl_Position = vec4(vertex, 1);\n"
     "}\n";
 
 static const char *fragmentShaderSourceCore =
     "#version 150\n"
-    "in highp vec3 vert;\n"
-    "in highp vec3 vertNormal;\n"
     "out highp vec4 fragColor;\n"
-    "uniform highp vec3 lightPos;\n"
     "void main() {\n"
-    "   highp vec3 L = normalize(lightPos - vert);\n"
-    "   highp float NL = max(dot(normalize(vertNormal), L), 0.0);\n"
-    "   highp vec3 color = vec3(0.39, 1.0, 0.0);\n"
-    "   highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);\n"
-    "   fragColor = vec4(col, 1.0);\n"
+    "   fragColor = vec4(1, 1, 1, 1);\n"
     "}\n";
 
 CameraMatricesWidget::CameraMatricesWidget(QWidget *parent)
@@ -52,13 +72,31 @@ QSize CameraMatricesWidget::sizeHint() const
 
 void CameraMatricesWidget::initializeGL()
 {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
     buildProgram();
+
+    _cameraMatrix.setToIdentity();
+    _cameraMatrix.translate(0, 0, -1);
 }
 
 void CameraMatricesWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
+
+    _worldMatrix.setToIdentity();
+
+    QOpenGLVertexArrayObject::Binder vaoBinder(&_vao);
+    _program->bind();
+
+//    _program->setUniformValue(_projectionMatrixLoc, _projectionMatrix);
+//    _program->setUniformValue(_modelviewMatrixLoc, _cameraMatrix * _worldMatrix);
+//    QMatrix3x3 normalMatrix = _worldMatrix.normalMatrix();
+//    _program->setUniformValue(_normalMatrixLoc, normalMatrix);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    _program->release();
 }
 
 void CameraMatricesWidget::resizeGL(int width, int height)
@@ -79,17 +117,31 @@ void CameraMatricesWidget::buildProgram()
     _program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSourceCore);
     _program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSourceCore);
     _program->bindAttributeLocation("vertex", 0);
-    _program->bindAttributeLocation("normal", 1);
+//    _program->bindAttributeLocation("normal", 1);
     _program->link();
 
     _program->bind();
-    _projectionMatrixLoc = _program->uniformLocation("projMatrix");
-    _modelviewMatrixLoc = _program->uniformLocation("mvMatrix");
-    _normalMatrixLoc = _program->uniformLocation("normalMatrix");
-    _lightPosLoc = _program->uniformLocation("lightPos");
+//    _projectionMatrixLoc = _program->uniformLocation("projMatrix");
+//    _modelviewMatrixLoc = _program->uniformLocation("mvMatrix");
+//    _normalMatrixLoc = _program->uniformLocation("normalMatrix");
+//    _lightPosLoc = _program->uniformLocation("lightPos");
 
-    _program->setUniformValue(_lightPosLoc, QVector3D(0, 0, 70));
+//    _program->setUniformValue(_lightPosLoc, QVector3D(0, 0, 70));
+
     _vao.create();
+    QOpenGLVertexArrayObject::Binder vaoBinder(&_vao);
+
+    _vbo.create();
+    _vbo.bind();
+
+    _vbo.allocate(data3d, sizeof(data3d));
+
+    QOpenGLFunctions* f = QOpenGLContext::currentContext()->functions();
+    f->glEnableVertexAttribArray(0);
+//    f->glEnableVertexAttribArray(1);
+    f->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+//    f->glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<void *>(3 * sizeof(GLfloat)));
+    _vbo.release();
 
     _program->release();
 }
--- cameramatriceswidget.h
+++ cameramatriceswidget.h
@@ -1,6 +1,7 @@
 #ifndef CAMERAMATRICESWIDGET_H
 #define CAMERAMATRICESWIDGET_H
 
+#include <QOpenGLBuffer>
 #include <QOpenGLVertexArrayObject>
 #include <QOpenGLWidget>
 #include <QMatrix4x4>
@@ -29,6 +30,7 @@ protected:
 private:
     QOpenGLShaderProgram* _program;
     QOpenGLVertexArrayObject _vao;
+    QOpenGLBuffer _vbo;
 
     QMatrix4x4 _projectionMatrix;
     QMatrix4x4 _worldMatrix;
