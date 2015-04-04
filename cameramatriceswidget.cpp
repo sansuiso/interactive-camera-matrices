f0038a260ec8ed72936794d7e9af1312c4fab337
Emmanuel D'ANGELO <emmanuel.dangelo@gmail.com>
2015-04-04 16:15:07 +0200

    Fix warning

--- cameramatriceswidget.cpp
+++ cameramatriceswidget.cpp
@@ -21,10 +21,12 @@ void CameraMatricesWidget::initializeGL()
 
 void CameraMatricesWidget::paintGL()
 {
-
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
 }
 
 void CameraMatricesWidget::resizeGL(int width, int height)
 {
-
+    Q_UNUSED(width);
+    Q_UNUSED(height);
 }
